7cca6acacfbd2043a239b62c5882c7bea4374a8e
FernandaPat <A01752828@tec.mx>
2025-03-09 18:22:29 -0600

    Movimiento personaje

---
+++ Assets/Scripts/CambiaAnimacion.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+/*
+Modifica los parametros del animator del personaje 
+Autor: Fernanda Pineda
+*/
+public class CambiaAnimacion : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer spRenderer;
+    private Animator animator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb= GetComponent<Rigidbody2D>();
+        spRenderer= GetComponent<SpriteRenderer>();
+        animator= GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Modificar el parámetro del animator "Velocidad"
+        animator.SetFloat("velocidad", Mathf.Abs(rb.linearVelocityX));
+        spRenderer.flipX = rb.linearVelocityX < 0;  
+        //animator.SetBool("enPiso", EstadoPersonaje.enPiso);
+    }
+}
