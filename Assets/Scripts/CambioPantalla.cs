0098c15dc666dcefb3080a04078652c3cd91ca08
Raúl Maldonado <A01276808@tec.mx>
2025-04-11 13:49:45 -0600

    Transiciones WIP

--- Assets/Scripts/CambioPantalla.cs
+++ Assets/Scripts/CambioPantalla.cs
@@ -3,13 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class CambioPantalla : MonoBehaviour
 {
-    [SerializeField] public string sceneToLoad = "Cutscene 1";
+    [SerializeField] private string sceneToLoad = "Cutscene 1";
+    [SerializeField] private Animator fadeAnimator; // Referencia al Animator
+    private bool activado = false; // Para evitar múltiples activaciones
 
-    public void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player")) 
+        if (!activado && other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            activado = true;
+            fadeAnimator.SetTrigger("FadeOut"); // Activa la animación
+            Invoke("CambiarEscena", 1f); // Espera el tiempo de la animación
         }
+    }
+
+    private void CambiarEscena()
+    {
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
