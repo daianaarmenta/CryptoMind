25957b1390fb2e0f7fefcd69192f99f6e8ca6f38
FernandaPat <A01752828@tec.mx>
2025-04-30 22:27:44 -0600

    Brillo arreglado

--- Assets/Scripts/UINiveles/LogicaBrillo.cs
+++ Assets/Scripts/UINiveles/LogicaBrillo.cs
@@ -4,25 +4,23 @@ using UnityEngine.UI;
 public class LogicaBrillo : MonoBehaviour
 {
     public Slider sliderBrillo; // Referencia al slider de brillo
-    public float sliderValue; // Valor del brillo
-    public Image panelBrillo; // Referencia al panel de opciones
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float sliderValue;   // Valor del brillo
+    public Image panelBrillo;   // Referencia al panel que oscurece o aclara la pantalla
+
     void Start()
     {
-       sliderBrillo.value = PlayerPrefs.GetFloat("brillo", 0.5f); // Carga el brillo guardado o establece el valor predeterminado en 0.5f
-       panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g,panelBrillo.color.b,sliderBrillo.value); // Establece el brillo inicial del panel de opciones
+        // Carga el valor de brillo guardado
+        sliderValue = PlayerPrefs.GetFloat("brillo", 0.5f);
+        sliderBrillo.value = sliderValue;
+
+        // Aplica el brillo invertido para que la izquierda sea oscuro
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, 1 - sliderValue);
     }
+
     public void CambiarBrillo(float valor)
     {
-        sliderValue = valor; // Actualiza el valor del brillo
-        PlayerPrefs.SetFloat("brillo", sliderValue); // Guarda el brillo en PlayerPrefs
-        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, 1 - sliderBrillo.value);
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        sliderValue = valor;
+        PlayerPrefs.SetFloat("brillo", sliderValue); // Guarda el valor
+        panelBrillo.color = new Color(panelBrillo.color.r, panelBrillo.color.g, panelBrillo.color.b, 1 - sliderValue);
     }
 }
